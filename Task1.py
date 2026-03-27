ca8f00755cbc086446c7340aa2c1a1a55145ff4e
Callum Howie <kwb21127@archie-e.hpc.strath.ac.uk>
2026-03-27 14:36:57 +0000

    adding shebang to the top of the python script and writing a brief doc string

---
+++ Task1.py
@@ -0,0 +1,12 @@
+#!/opt/software/anaconda/python-3.10.9/bin/python
+"""
+2D Ising model simulation
+
+This program implements the classical nearest-neighbour 2D ISing model on an L x L
+square lattice with periodic boundary conditions.
+
+Tested using:
+    Python 3.10.9
+"""
+import numpy as np 
+
