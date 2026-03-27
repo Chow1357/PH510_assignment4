0fa1277f89588735a3bdb0956eda24eaff75ceea
Callum Howie <kwb21127@archie-e.hpc.strath.ac.uk>
2026-03-27 14:52:59 +0000

    creating the lattice and adding random spin states at the different lattice sites

--- Task1.py
+++ Task1.py
@@ -2,12 +2,27 @@
 """
 2D Ising model simulation
 
-This program implements the classical nearest-neighbour 2D ISing model on an L x L
-square lattice with periodic boundary conditions.
+This program implements the classical nearest-neighbour 
+2D ISing model on an L x L square lattice with 
+periodic boundary conditions.
 
 Tested using:
     Python 3.10.9
 """
 import numpy as np 
 
-def initialise_lattice(L, ordered=False, seed=None)
+#size is equivalent to L for grid size
+def initialise_lattice(size, ordered=False, seed=1234):
+    """
+    Create an LxL (sizexsize) Ising spin Lattice.
+    """
+    rng = np.random.default_rng(seed)
+
+    if ordered:
+        return np.ones((size, size), dtype=int)
+
+    return rng.choice([-1, 1], (size, size))
+
+
+def total_energy(spins, J=1.0)
+
