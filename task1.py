8d0b307681322b9e69aca6c50dd818104a50b4ba
Callum Howie <kwb21127@archie-t.hpc.strath.ac.uk>
2026-03-30 12:53:37 +0100

    making pylint chnages and adding print functions

--- task1.py
+++ task1.py
@@ -24,56 +24,75 @@ def initialise_lattice(size, ordered=False, seed=1234):
     return rng.choice([-1, 1], (size, size))
 
 
-def total_energy(spins, j_val=1.0):
+def total_energy(lattice, j_val=1.0):
     """
     Computing total energy of the lattice using nearest-neighbour interactions
     """
 
-    g_size = spins.shape[0]
+    l = lattice.shape[0]
     energy = 0.0
 
-    for i in range(L):
-        for j in range(L):
+    for i in range(l):
+        for j in range(l):
             s = lattice[i, j]
 
             # periodic neighbours
-            right = lattice[i, (j + 1) % L]
-            down = lattice[(i + 1) % L, j]
+            right = lattice[i, (j + 1) % l]
+            down = lattice[(i + 1) % l, j]
 
             energy += -j_val * s * (right + down)
 
     return energy
 
-def delta_energy(spins, i, j, j_val=1.0):
+def delta_energy(spins, i, j, j_val):
     """
     function which finds the value of the system 
     upon flipping one spin
     """
-    L = spins.shape(0)
-    S = spins[i, j]
+    l = spins.shape[0]
+    s = spins[i, j]
 
     neighbour_sum = (
-        spins[(i + 1) % L, j] +
-        spins[(i - 1) % L, j] +
-        spins[i, (j + 1) % L] +
-        spins[i, (j - 1) % L]
+        spins[(i + 1) % l, j] +
+        spins[(i - 1) % l, j] +
+        spins[i, (j + 1) % l] +
+        spins[i, (j - 1) % l]
     )
     # returning energy chnage for one spin change
     return 2.0 * j_val * s * neighbour_sum
 
-def magnetisation(spins):
+def magnetisation(spin):
     """
     Compute the total magnetisation.
     How alligned the entire system is.
     """
     # adding up all the spins to check allignment
-    return np.sum(spins)
+    return np.sum(spin)
 
 # ensuring this part will only run
 # if this file is executed directly
 if __name__ == "__main__":
     # setting parameters
     L = 8
-    j_val = 1.0
+    J_VAL = 1.0
 
-    spins = initialise_lattice(L, ordered=False, seed=1234)
+    # create a random lattice with a seed so reproducible
+    spin_lattice = initialise_lattice(L, ordered=False, seed=1234)
+
+    # calling functions for magentisation and energy
+    total_e = total_energy(spin_lattice, J_VAL)
+    mag = magnetisation(spin_lattice)
+
+    # printing results
+    print("Random spin lattice:")
+    print(spin_lattice)
+    print()
+    print(f"Total energy: {total_e}")
+    print(f"Magnetisation: {mag}")
+    print(f"Magnetisation per site: {mag / (L * L):.6f}")
+    print(f"Energy per site; {total_e / (L * L):.6f}")
+
+    ordered_spins = initialise_lattice(L, ordered=True)
+    print("Ordered lattice test:")
+    print(f"Total energy: {total_energy(ordered_spins, J_VAL)}")
+    print(f"Magnetisation: {magnetisation(ordered_spins)}")
